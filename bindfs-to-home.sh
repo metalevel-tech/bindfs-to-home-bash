bfbd844192a3b7a50bf711e3354bcb7a437a5a3e
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2023-05-14 11:58:19 +0300

    fallback for the default src dir: /var/www/ ?? /var/etc/
    output help and vars on wrong input

--- bindfs-to-home.sh
+++ bindfs-to-home.sh
@@ -6,18 +6,26 @@
 #
 # @name         bindfs-to-home.sh
 
-[[ -z ${1+x} ]] && DO_ACTION="L-V-H"     || DO_ACTION="$1"   # Default action LIST MOUNTED, VERBOSE OUTPUT
-[[ -z ${2+x} ]] && SOURCE_DIR="/var/www" || SOURCE_DIR="$2"  # Default target directory '/var/www'
-[[ -z ${3+x} ]] && SOURCE_USR="AUTO"     || SOURCE_USR="$3"  # Default source user is the user owned the target dir
+[[ -z ${1+x} ]] && DO_ACTION="" || DO_ACTION="$1"       # Default action LIST MOUNTED, VERBOSE OUTPUT
+[[ -z ${2+x} ]] && SOURCE_DIR="" || SOURCE_DIR="$2"     # Default target directory '/var/www'
+[[ -z ${3+x} ]] && SOURCE_USR="AUTO" || SOURCE_USR="$3" # Default source user is the user owned the target dir
+
+if [[ ! -d $SOURCE_DIR ]]; then
+    if [[ -d /var/www ]]; then
+        SOURCE_DIR="/var/www" # Fallback of the default directory
+    else
+        SOURCE_DIR="/etc" # Fallback of the default directory
+    fi
+fi
 
 RELAT_PATH="${HOME}/bindfs"
 TARGET_DIR="${RELAT_PATH}${SOURCE_DIR}"
 
 function list_all_mounted_in_relative_path() {
-        printf "List of the directories mounted under '%s':\n\033[1;33m%s\n" \
-            "${RELAT_PATH}" \
-            "$(findmnt -nrt fuse -o TARGET | grep "$RELAT_PATH" || echo 'No mounted directories!')"
-        printf '\033[0m'
+    printf "List of the directories mounted under '%s':\n\033[1;33m%s\n" \
+        "${RELAT_PATH}" \
+        "$(findmnt -nrt fuse -o TARGET | grep "$RELAT_PATH" || echo 'No mounted directories!')"
+    printf '\033[0m'
 }
 
 function target_dir_unmount() {
@@ -26,16 +34,13 @@ function target_dir_unmount() {
 
 function unmount_all_in_relative_path() {
     echo ''
-    for DIR in $(findmnt -nrt fuse -o TARGET | grep "$RELAT_PATH")
-    do
-        if [[ $DIR == '' ]]
-        then
+    for DIR in $(findmnt -nrt fuse -o TARGET | grep "$RELAT_PATH"); do
+        if [[ $DIR == '' ]]; then
             return false
         else
             sudo fusermount -u "$DIR"
 
-            if [[ $? -eq 0 ]]
-            then
+            if [[ $? -eq 0 ]]; then
                 printf '\033[1;33m%s\033[0m is unmounted.\n' "$DIR"
             else
                 printf '\033[1;33mSomething went wrong!\033[0m\n' "$DIR"
@@ -50,14 +55,13 @@ function target_dir_create() {
 
 function target_dir_mount() {
     sudo bindfs -u "$TGT_USR_UID" -g "$TGT_USR_GID" \
-                --create-for-user="$SRC_USR_UID" \
-                --create-for-group="$SRC_USR_GID" \
-                "$SOURCE_DIR" "$TARGET_DIR"
+        --create-for-user="$SRC_USR_UID" \
+        --create-for-group="$SRC_USR_GID" \
+        "$SOURCE_DIR" "$TARGET_DIR"
 }
 
 function get_users() {
-    if [[ ${SOURCE_USR^^} == 'AUTO' ]]
-    then
+    if [[ ${SOURCE_USR^^} == 'AUTO' ]]; then
         SRC_USR_UID=$(stat -c '%u' "${SOURCE_DIR}")
         SRC_USR_GID=$(stat -c '%g' "${SOURCE_DIR}")
     else
@@ -79,7 +83,7 @@ function print_vars() {
     echo -e "\tSRC_USR_GID: $SRC_USR_GID\n"
     echo -e "\tTGT_USR_UID: $TGT_USR_UID"
     echo -e "\tTGT_USR_GID: $TGT_USR_GID\n"
-    echo -e "\tDO_ACTION: ${DO_ACTION^^}\n"
+    echo -e "\tDO_ACTION: [${DO_ACTION^^}] \033[1;33m${1}\033[0m\n"
 }
 
 function get_help() {
@@ -93,53 +97,53 @@ function get_help() {
     echo -e "\t$(basename ${0}) L"
     echo -e "\t$(basename ${0}) Mount [/target/dir] [user]"
     echo -e "\n\t m: mount\n\t u: un mount\n\t a: un mount all [default]\n\t l: list all mounted directories"
-    echo -e "\n\t-v: print the variables\n\t-h: print the help\n"
+    echo -e "\n\t-v|v: print the variables\n\t-h|h: print the help\n"
 }
 
 function do_action() {
+    # Output the help and the current vars
+    # if there isn't provided a correct option
+    if [[ ! ${DO_ACTION^^} =~ ^(\-\-H|\-H|H|\-\-V|\-V|V|M|U|L) ]]; then
+        get_help
+        print_vars "Not supported!"
+        exit
+    fi
+
     # Pseudo options process ---
-    if [[ ${DO_ACTION^^} =~ ^(\-\-|\-)H ]]
-    then
-        [[ ${DO_ACTION^^} =~ (\-\-|\-)V ]] && print_vars
+    if [[ ${DO_ACTION^^} =~ ^(\-\-|\-|)H ]]; then
+        [[ ${DO_ACTION^^} =~ (\-\-|\-|)V ]] && print_vars
         get_help
         exit
     fi
 
-    if [[ ${DO_ACTION^^} =~ ^(\-\-|\-)V ]]
-    then
+    if [[ ${DO_ACTION^^} =~ ^(\-\-|\-|)V ]]; then
         print_vars
-        [[ ${DO_ACTION^^} =~ (\-\-|\-)H ]] && get_help
+        [[ ${DO_ACTION^^} =~ (\-\-|\-|)H ]] && get_help
         exit
-    elif [[ ${DO_ACTION^^} =~ (\-\-|\-)V ]]
-    then
+    elif [[ ${DO_ACTION^^} =~ (\-\-|\-|)V ]]; then
         print_vars
-        [[ ${DO_ACTION^^} =~ (\-\-|\-)H ]] && get_help
+        [[ ${DO_ACTION^^} =~ (\-\-|\-|)H ]] && get_help
     fi
     # ---
 
-    if [[ ${DO_ACTION^^} =~ M ]]
-    then
+    if [[ ${DO_ACTION^^} =~ M ]]; then
         target_dir_unmount # try to unmount target dir just in case
         target_dir_create  # try to make the target dir just in case
         target_dir_mount   # finally do the bindfs mount
-    elif [[ ${DO_ACTION^^} =~ U ]]
-    then
-        if target_dir_unmount
-        then
+    elif [[ ${DO_ACTION^^} =~ U ]]; then
+        if target_dir_unmount; then
             printf '\033[1;33m%s\033[0m is unmounted.\n' "$TARGET_DIR"
         else
             printf '\033[1;33m%s\033[0m was not mounted.\n' "$TARGET_DIR"
         fi
-    elif [[ ${DO_ACTION^^} =~ L ]]
-    then
+    elif [[ ${DO_ACTION^^} =~ L ]]; then
         list_all_mounted_in_relative_path
     else
         list_all_mounted_in_relative_path
 
         # We do not need [[ ${DO_ACTION^^} =~ A ]]
         # because will fall here if it is not M|U|L
-        if ! unmount_all_in_relative_path
-        then
+        if ! unmount_all_in_relative_path; then
             list_all_mounted_in_relative_path
         fi
     fi
